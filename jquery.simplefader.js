0258c9f24c0e16a2f83249c75cac7086f6e6c3a3
Miles Rausch <miles.rausch@securitylabs.com>
2012-11-15 10:21:24 -0600

    All the stuff
    Created demo page.
    Completed plugin.
    Created readme.

--- jquery.simplefader.js
+++ jquery.simplefader.js
@@ -1,32 +1,46 @@
-/*jslint devel: false, browser: false, white: true */
+/*jslint devel: false, browser: true, white: true */
 /*global $: true, jQuery: true */
 (function( $ ){
+    "use strict";
+
     $.simplefader = function( el, numberOfImages, options ){
         var base = this,
             timer = 0,
             index = 0,
+            imagePathParts = {},
+            parse = '',
             imageUpdate = '',
             imageLoad = function() {
-                $( this ).fadeIn();
+                $( this ).fadeIn( base.options.animationSpeed );
             };
         
         base.$el = $( el );
         base.el = el;
         
         base.$el.data( "simplefader", base );
-        
-        base.init = function(){
-            var img = base.$el.find('img:first-child');
-            
-            img.bind( 'load', imageLoad );
 
-            if( typeof( numberOfImages ) === "undefined" || numberOfImages === null ) numberOfImages = 1;
-            
-            base.numberOfImages = numberOfImages;
-            
-            base.options = $.extend( {},$.simplefader.defaultOptions, options );
+        parse = function( url ) {
+            var separator = '/',
+                reDigit = /\d/,
+                urlAsArray = [],
+                parts = {
+                    directory: '',
+                    fileName: '',
+                    filePattern: 'image#num#.jpg'
+                };
 
-            imageUpdate();
+            if ( url.indexOf( separator ) === -1 ) {
+                separator = '\\';
+            }
+
+            urlAsArray = url.split( separator );
+            parts.fileName = urlAsArray.pop();
+            parts.filePattern = parts.fileName.replace( reDigit, '#num#' );
+            parts.directory = urlAsArray.join( separator ) + separator;
+
+            index = parseInt( parts.fileName.match( reDigit )[ 0 ], 10 ) - 1;
+
+            return parts;
         };
 
         imageUpdate = function() {
@@ -34,6 +48,8 @@
                 img = base.$el.find('img:first-child');
 
             timer = window.setTimeout(function() {
+                var newindex = 0;
+
                 index = index + 1;
                 
                 if ( index >= numberOfImages ) {
@@ -42,7 +58,9 @@
                      index = numberOfImages - 1;
                 }
                 
-                newRotatorImage = '/lib/images/slideshow'  + String( index % numberOfImages + 1 ) + '.jpg';
+                newindex = String( index % numberOfImages + 1 );
+
+                newRotatorImage = imagePathParts.directory + imagePathParts.filePattern.replace( '#num#', newindex );
 
                 img.fadeOut( base.options.animationSpeed, function() {
                     img.attr( 'src', newRotatorImage );
@@ -54,17 +72,34 @@
             }, base.options.speed );
         };
         
+        base.init = function() {
+            var img = base.$el.find('img:first-child');
+
+            imagePathParts = parse( img.attr('src') );
+
+            if( typeof( numberOfImages ) === "undefined" || numberOfImages === null ) {
+                numberOfImages = 1;
+            }
+
+            base.numberOfImages = numberOfImages;
+            base.options = $.extend( {}, $.simplefader.defaultOptions, options );
+
+            img.bind( 'load', imageLoad );
+
+            imageUpdate();
+        };
+        
         base.init();
     };
     
     $.simplefader.defaultOptions = {
         speed: 5000,
-        animationSpeed: 300
+        animationSpeed: 600
     };
     
     $.fn.simplefader = function(numberOfImages, options){
         return this.each(function(){
-            (new $.simplefader(this, numberOfImages, options));
+            ( new $.simplefader(this, numberOfImages, options) );
         });
     };
     
@@ -72,4 +107,4 @@
         this.data("simplefader");
     };
     
-})( jQuery );
+}( jQuery ));
---
+++ readme.md
@@ -0,0 +1,39 @@
+# jSimpleFader Demo Page #
+
+By [Miles Rausch](http://milesrausch.com)
+
+jSimpleFader is a jQuery plugin to simply fade between images.
+
+```javascript
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="jquery.simplefader.js"></script>
+```
+
+To use your fader, you simply call it on the element containing the image you want to rotate. That image doesn't have to be the only HTML in the element &ndash; the script will use the first image it finds. The only thing you need to provide is the total number of images you want to rotate through. The script assumes you've numbered your images like: "image1.jpg", "image2.jpg", "image3.jpg", &amp;c.
+
+```javascript
+<div id="fader1">
+    <span><img src="images/fader1/1.jpg"></span>
+</div>
+<script>
+    $('#fader1').simplefader( 4 );
+</script>
+```
+
+The image you provide doesn't have to be the first image in your series, but you do need to have images starting at 1. It will start fading from the image you provide, working in order.
+    
+You can optionally pass in an options object. Here are the options you can set:
+
+  * speed &ndash; the speed of rotation (default: 5000 ms)
+  * animationSpeed &ndash; the speed of fading (default: 600)
+
+```javascript
+<div id="fader2">
+    <img src="images/fader2/image3.jpg">
+</div>
+<script>
+    $('#fader2').simplefader( 4, { speed: 2500, animationSpeed: 300 } );
+</script>
+```
+
+Browse the `demo.html` file to see it in action.
