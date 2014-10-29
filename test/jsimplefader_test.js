8de302b3790fa3aa8e580c6aac58c86025cbb835
Miles Rausch <miles@milesrausch.com>
2014-10-29 16:41:39 -0400

    Expanded tests
    There’s still a non-passing test, but I wanted to commit this stuff
    before I made further changes.

--- test/jsimplefader_test.js
+++ test/jsimplefader_test.js
@@ -1,24 +1,89 @@
-(function($) {
+(function( $ ) {
 
   module('jQuery#simplefader', {
-    // This will run before each test in this module.
     setup: function() {
       this.fader1 = $('#fader1').simplefader( 4 );
-      this.fader2 = $('#fader2').simplefader( 4, { speed: 2500, animationSpeed: 300, animationStyle: 'crossfade' } );
-      this.fader3 = $('#fader3').simplefader( 4, { links: [ 'http://google.com', 'http://apple.com', 'http://microsoft.com' ] } );
     }
   });
 
   test('can attach', function() {
-    expect(3);
+    expect( 1 );
+
     equal( this.fader1.length, 1 );
-    equal( this.fader2.length, 1 );
-    equal( this.fader3.length, 1 );
   });
 
-  test('can get simplefader', function() {
+  test('can get itself', function() {
     expect( 1 );
+
     equal( $('#fader1').getsimplefader().$el[ 0 ], this.fader1[ 0 ] );
   });
 
-}(jQuery));
+
+
+
+  module('jQuery#simplefader with speed options', {
+    setup: function() {
+      var fader2 = $('#fader2');
+
+      this.fader2image = fader2.find('img');
+      this.fader2 = fader2.simplefader( 4, { speed: 2500, animationSpeed: 300, animationStyle: 'crossfade' } );
+    }
+  });
+
+  test('can attach', function() {
+    expect( 1 );
+
+    equal( this.fader2.length, 1 );
+  });
+
+  asyncTest('can rotate between images', function() {
+    var self = this;
+
+    expect( 1 );
+
+    setTimeout(function() {
+      var newImage = self.fader2.find('img').attr('src'),
+        originalImage = self.fader2image.attr('src');
+
+      notEqual( originalImage, newImage );
+
+      start();
+    }, 2900 );
+  });
+
+
+
+
+  module('jQuery#simplefader with link options', {
+    setup: function() {
+      var fader3 = $('#fader3');
+
+      this.fader3link = fader3.find('a');
+      this.fader3 = fader3.simplefader( 4, { links: [ 'http://google.com', 'http://apple.com', 'http://microsoft.com' ] } );
+    }
+  });
+
+  test('can attach', function() {
+    expect( 1 );
+
+    equal( this.fader3.length, 1 );
+  });
+
+  asyncTest('can rotate between images', function() {
+    var self = this;
+
+    expect( 1 );
+
+    setTimeout(function() {
+      var newLink = self.fader3.find('a').attr('href'),
+        originalLink = self.fader3link.attr('href');
+
+      //console.log( originalLink, newLink );
+
+      notEqual( originalLink, newLink );
+
+      start();
+    }, 5800 );
+  });
+
+}( jQuery ));
